f41d331caef1b7e566cad77d537125f90f5060d2
Philipp Meier <p1meier@hsr.ch>
2013-11-18 15:00:25 +0100

    Add: Voting for Posts via Ajax

--- webapp/src/main/java/ch/hsr/dpreddit/webapp/PostBean.java
+++ webapp/src/main/java/ch/hsr/dpreddit/webapp/PostBean.java
@@ -2,28 +2,17 @@ package ch.hsr.dpreddit.webapp;
 
 import ch.hsr.dpreddit.spa.DPRedditDB;
 import ch.hsr.dpreddit.spa.Post;
-import ch.hsr.dpreddit.spa.User;
 
-import javax.faces.application.FacesMessage;
+import javax.enterprise.context.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import java.util.Set;
 
 @ManagedBean(name="postbean")
+@ApplicationScoped
 public class PostBean {
 
     public Set<Post> getPosts() {
         return DPRedditDB.getInstance().getPostSet();
-    }
-
-    public String votePost(Post post) {
-        UserSessionBean userSessionBeanBean = (UserSessionBean)FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("userSessionBean");
-
-        if(userSessionBeanBean.isLoggedIn()) {
-            System.out.println( post.vote(userSessionBeanBean.getUser()));
-        }
-
-        FacesContext.getCurrentInstance().renderResponse();
-        return "/index";
     }
 }
